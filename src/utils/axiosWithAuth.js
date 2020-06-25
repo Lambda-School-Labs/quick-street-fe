ef454d6b18f7ed6e72e8e6e8ef56f81188601682
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-06-24 23:35:56 -0600

    Had to correct the baseURL from localhost to production.

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,8 +4,8 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
 
   return axios.create({
-    baseURL: "http://localhost:5000/api",
-    // baseURL: "https://market-avenue.herokuapp.com/api",
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "https://market-avenue.herokuapp.com/api",
     headers: {
       Authorization: token,
       "Content-Type": "application/json",
