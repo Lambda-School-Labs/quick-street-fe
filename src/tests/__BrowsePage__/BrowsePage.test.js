6553d949eb6bcd12c6631dbbef50b0130fb84086
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-07-09 00:31:08 -0600

    adding more and more tests. This time for the browse page.

---
+++ src/tests/__BrowsePage__/BrowsePage.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  render,
+  fireEvent,
+  waitForElement,
+  getByTestId,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { Provider as AuthProvider } from "../../contexts/AuthContext";
+import { Provider as CartProvider } from "../../contexts/TestCartContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import Browse from "../../pages/Browse";
+
+describe("running tests on login", () => {
+  const tree = (
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <Browse />
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  );
+  it(`full component renders`, () => {
+    const { getByTestId, getAllByText } = render(tree);
+
+    expect(getByTestId("browse-wrapper")).toBeInTheDocument();
+  });
+});
