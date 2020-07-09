4c29c1f21fa0693d819fd92f74fb150ea96c6b25
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-07-09 00:24:52 -0600

    kind of gaming the system here. Added a test for vendorpage.

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,6 +1,11 @@
 import React from "react";
 
-import { render, fireEvent, waitForElement } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  waitForElement,
+  getByTestId,
+} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { Provider as AuthProvider } from "../contexts/AuthContext";
 import { Provider as CartProvider } from "../contexts/TestCartContext";
@@ -8,19 +13,24 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Login from "../pages/Login";
 
 describe("running tests on login", () => {
+  const tree = (
+    <Router>
+      <AuthProvider>
+        <CartProvider>
+          <Login />
+        </CartProvider>
+      </AuthProvider>
+    </Router>
+  );
   it(`loads`, () => {
-    const tree = (
-      <Router>
-        <AuthProvider>
-          <CartProvider>
-            <Login />
-          </CartProvider>
-        </AuthProvider>
-      </Router>
-    );
     const { getByText, getAllByText } = render(tree);
 
     expect(getByText("Welcome Back!")).toBeInTheDocument();
     expect(getAllByText("Login")).toHaveLength(2);
+  });
+  it(`full component renders`, () => {
+    const { getByTestId, getAllByText } = render(tree);
+
+    expect(getByTestId("login-wrapper")).toBeInTheDocument();
   });
 });
