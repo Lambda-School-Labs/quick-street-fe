504b9c4b5bf1376320b75537eda33554032d1026
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-07-15 21:33:23 -0600

    Set up the edit functionality, needs styling.

--- src/customer-flow/components/CustomerProfile.js
+++ src/customer-flow/components/CustomerProfile.js
@@ -7,6 +7,7 @@ const CustomerForm = () => {
     phone_number: "",
     zip_code: "",
   });
+  const [editing, setEditing] = useState(false);
 
   const changeHandler = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -17,7 +18,10 @@ const CustomerForm = () => {
     console.log("formData", formData);
     axiosWithAuth()
       .put("/customers/profile/update", formData)
-      .then((res) => console.log("res from customer update", res))
+      .then((res) => {
+        console.log("res from customer update", res);
+        editField();
+      })
       .catch((err) => console.log(err));
   };
 
@@ -31,40 +35,54 @@ const CustomerForm = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  function editField() {
+    setEditing(!editing);
+  }
+
   return (
     <div>
       <h1> Your Profile</h1>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="customer_name">Name</label>
-        <input
-          name="customer_name"
-          type="text"
-          value={formData.customer_name}
-          onChange={changeHandler}
-        />
-        <label htmlFor="address">Address</label>
-        <input
-          name="address"
-          type="text"
-          value={formData.address}
-          onChange={changeHandler}
-        />
-        <label htmlFor="phone_number">Phone Number</label>
-        <input
-          name="phone_number"
-          type="text"
-          value={formData.phone_number}
-          onChange={changeHandler}
-        />
-        <label htmlFor="zip_code">Zip Code</label>
-        <input
-          name="zip_code"
-          type="text"
-          value={formData.zip_code}
-          onChange={changeHandler}
-        />
-        <button type="submit">Update</button>
-      </form>
+      {editing ? (
+        <form onSubmit={submitHandler}>
+          <label htmlFor="customer_name">Name</label>
+          <input
+            name="customer_name"
+            type="text"
+            value={formData.customer_name}
+            onChange={changeHandler}
+          />
+          <label htmlFor="address">Address</label>
+          <input
+            name="address"
+            type="text"
+            value={formData.address}
+            onChange={changeHandler}
+          />
+          <label htmlFor="phone_number">Phone Number</label>
+          <input
+            name="phone_number"
+            type="text"
+            value={formData.phone_number}
+            onChange={changeHandler}
+          />
+          <label htmlFor="zip_code">Zip Code</label>
+          <input
+            name="zip_code"
+            type="text"
+            value={formData.zip_code}
+            onChange={changeHandler}
+          />
+          <button type="submit">Update</button>
+          <button onClick={editField}>Cancel</button>
+        </form>
+      ) : (
+        <div onClick={editField}>
+          <h3>{formData.customer_name}</h3>
+          <h3>{formData.address}</h3>
+          <h3>{formData.phone_number}</h3>
+          <h3>{formData.zip_code}</h3>
+        </div>
+      )}
     </div>
   );
 };
