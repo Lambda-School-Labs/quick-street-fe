6463c088407656fa0bba5c7866bd3a9cdca26f9e
bsherwood9 <36565536+bsherwood9@users.noreply.github.com>
2020-07-16 21:56:28 -0600

    Slowly started designing the customer profile page and adding svgs.

---
+++ src/assets/svgs/customerflow/index.js
@@ -0,0 +1,13 @@
+import dashboardIcon from "./dashboard.svg";
+import profileIcon from "./profile_icon.svg";
+import dailyIcon from "./daily.svg";
+import ordersIcon from "./orders.svg";
+import logoImg from "./Logo.svg";
+import logOut from "./log_out.svg";
+
+export const logout = logOut;
+export const orders = ordersIcon;
+export const logo = logoImg;
+export const daily = dailyIcon;
+export const dashboard_icon = dashboardIcon;
+export const profile_icon = profileIcon;
--- src/customer-flow/components/CustomerPage.js
+++ src/customer-flow/components/CustomerPage.js
@@ -1,10 +1,43 @@
 import React from "react";
 import CustomerProfile from "../components/CustomerProfile";
+import customerProfile from "../../styles/css/customer/customer_profile_page.css";
+import {
+  dashboard_icon,
+  profile_icon,
+  daily,
+  orders,
+  logo,
+  logout,
+} from "../../assets/svgs/customerflow";
 const CustomerPage = () => {
   return (
-    <div>
-      <h1>Customer Page</h1>
-      <CustomerProfile />
+    <div className="page-wrapper">
+      <div className="side-nav">
+        <img src={logo} alt="logo" />
+        <ul>
+          <li>
+            <img src={dashboard_icon} alt="dashboard icon" />
+            Dashboard
+          </li>
+          <li>
+            <img src={profile_icon} alt="dashboard icon" />
+            Profile
+          </li>
+          <li>
+            <img src={orders} alt="orders icon" />
+            Orders
+          </li>
+          <li>
+            <img src={daily} alt="daily icon" />
+            Favorites
+          </li>
+        </ul>
+        <img src={logout} alt="logout img" />
+        <h3>Log out</h3>
+      </div>
+      <div className="component-section">
+        <CustomerProfile />
+      </div>
     </div>
   );
 };
