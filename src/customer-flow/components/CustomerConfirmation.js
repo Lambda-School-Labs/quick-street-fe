de334fc4eb9a0e4844f2b10db599017d45b9222f
Ali Sheikh <ali.sheikh@flexport.com>
2020-08-06 15:40:36 -0700

    cleaning up

--- src/customer-flow/components/CustomerConfirmation.js
+++ src/customer-flow/components/CustomerConfirmation.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link, NavLink, Route } from "react-router-dom";
 import axiosWithAuth from "../../utils/axiosWithAuth";
 import "../../styles/css/customer/customer_favorites.css";
-import Table from "react-bootstrap/Table";
-import Button from "react-bootstrap/Button";
 import {
   Image,
   CloudinaryContext,
@@ -11,9 +9,6 @@ import {
 } from "cloudinary-react";
 
 const CustomerConfirmation = ({ name }) => {
-  const [favorites, setFavorites] = useState([]);
-  console.log("FAVORITES", favorites);
-
 
   return (
     <div className="favorites-wrapper">
@@ -21,7 +16,7 @@ const CustomerConfirmation = ({ name }) => {
 
       <div className="fav-info">
         <div>
-          <h1>Thanks for ordering</h1>
+          <h1>Thanks for ordering!</h1>
         </div>
       </div>
     </div>
