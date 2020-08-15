a2c4ce1def098d2f27a0787e057f88683847a3a9
Omid Ashtari <oashtari@gmail.com>
2020-08-14 17:12:01 -0700

    stuff that seems need committing

--- src/components/shared/ShoppingCart/ShoppingCartItem.js
+++ src/components/shared/ShoppingCart/ShoppingCartItem.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
+// import React from 'react';
+// import { Image, CloudinaryContext, Transformation } from 'cloudinary-react';
 
-// styles
-import shopping from '../../../styles/scss/shopping.module.scss';
+// // styles
+// import shopping from '../../../styles/scss/shopping.module.scss';
 
-const ShoppingCartItem = ({ product }) => {
-	return (
-		<React.Fragment>
-			<div className={shopping.wrapper}>
-				<div className={shopping.left}>
-					{/* {console.log(product.item.product_image.thumbnail_url)} */}
-					{product.item.product_image && (
-						<CloudinaryContext cloudName="quickstlabs">
-							<Image publicId={product.item.product_image.public_id}>
-								<Transformation gravity="center" height="92" width="110" crop="fill" />
-							</Image>
-						</CloudinaryContext>
-					)}
-				</div>
-				<div className={shopping.right}>
-					<p>
-						{product.quantity} {product.item.name}
-					</p>
-					<p>
-						${product.item.price} per {product.item.unit}
-					</p>
-				</div>
-			</div>
-		</React.Fragment>
-	);
-};
+// const ShoppingCartItem = ({ product }) => {
+// 	return (
+// 		<React.Fragment>
+// 			<div className={shopping.wrapper}>
+// 				<div className={shopping.left}>
+// 					{/* {console.log(product.item.product_image.thumbnail_url)} */}
+// 					{product.item.product_image && (
+// 						<CloudinaryContext cloudName="quickstlabs">
+// 							<Image publicId={product.item.product_image.public_id}>
+// 								<Transformation gravity="center" height="92" width="110" crop="fill" />
+// 							</Image>
+// 						</CloudinaryContext>
+// 					)}
+// 				</div>
+// 				<div className={shopping.right}>
+// 					<p>
+// 						{product.quantity} {product.item.name}
+// 					</p>
+// 					<p>
+// 						${product.item.price} per {product.item.unit}
+// 					</p>
+// 				</div>
+// 			</div>
+// 		</React.Fragment>
+// 	);
+// };
 
-export default ShoppingCartItem;
+// export default ShoppingCartItem;
--- src/components/shared/ShoppingCart/ShoppingCartItems.js
+++ src/components/shared/ShoppingCart/ShoppingCartItems.js
@@ -1,27 +1,27 @@
-import React, { useContext, useEffect } from "react";
-// import { Link } from 'react-router-dom';
-import { Context as CartContext } from "../../../contexts/TestCartContext";
-import { ShoppingCartItem } from "../../index";
+// import React, { useContext, useEffect } from "react";
+// // import { Link } from 'react-router-dom';
+// import { Context as CartContext } from "../../../contexts/TestCartContext";
+// import { ShoppingCartItem } from "../../index";
 
-const ShoppingCartItems = ({ setCartModal }) => {
-  const { state, getCartItems } = useContext(CartContext);
-  // console.log('state in shopping cart items', state);
-  const cart = state.cart;
-  const customerId = localStorage.getItem("user_id");
+// const ShoppingCartItems = ({ setCartModal }) => {
+//   const { state, getCartItems } = useContext(CartContext);
+//   // console.log('state in shopping cart items', state);
+//   const cart = state.cart;
+//   const customerId = localStorage.getItem("user_id");
 
-  useEffect(() => {
-    getCartItems(customerId);
-  }, []); // removed [] dependency
+//   useEffect(() => {
+//     getCartItems(customerId);
+//   }, []); // removed [] dependency
 
-  return (
-    <React.Fragment>
-      {cart &&
-        cart.items &&
-        cart.items.map((product) => (
-          <ShoppingCartItem product={product} key={product.item._id} />
-        ))}
-    </React.Fragment>
-  );
-};
+//   return (
+//     <React.Fragment>
+//       {cart &&
+//         cart.items &&
+//         cart.items.map((product) => (
+//           <ShoppingCartItem product={product} key={product.item._id} />
+//         ))}
+//     </React.Fragment>
+//   );
+// };
 
-export default ShoppingCartItems;
+// export default ShoppingCartItems;
